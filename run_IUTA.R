75ade4e11c947090b8136e4c1dfb30c71c1eea20
upendrak <upendra_35@yahoo.com>
2016-06-04 15:56:05 -0700

    add fld and test type conditionals

--- run_IUTA.R
+++ run_IUTA.R
@@ -1,7 +1,9 @@
 #!/usr/bin/Rscript
 
 # Rscript run_IUTA.R -h
-# Rscript run_IUTA.R --gtf test_data/mm10_kg_sample_IUTA.gtf --bam1 test_data/sample_1.bam,test_data/sample_2.bam,test_data/sample_3.bam --bam2 test_data/sample_4.bam,test_data/sample_5.bam,test_data/sample_6.bam --output test_data/new_ouput_test  --groups 4,5 --gene.id Pcmtd1
+# Rscript run_IUTA.R --gtf test_data/mm10_kg_sample_IUTA.gtf --bam1 test_data/sample_1.bam,test_data/sample_2.bam,test_data/sample_3.bam --bam2 test_data/sample_4.bam,test_data/sample_5.bam,test_data/sample_6.bam --fld empirical --test.type SKK,CQ,KY --output test_data/new_ouput_test2  --groups 4,5 --gene.id Pcmtd1
+# Rscript run_IUTA.R --gtf test_data/mm10_kg_sample_IUTA.gtf --bam1 test_data/sample_1.bam,test_data/sample_2.bam,test_data/sample_3.bam --bam2 test_data/sample_4.bam,test_data/sample_5.bam,test_data/sample_6.bam --test.type SKK,CQ,KY --output test_data/new_ouput_test2  --groups 4,5 --gene.id Pcmtd1
+# Rscript run_IUTA.R --gtf test_data/mm10_kg_sample_IUTA.gtf --bam1 test_data/sample_1.bam,test_data/sample_2.bam,test_data/sample_3.bam --bam2 test_data/sample_4.bam,test_data/sample_5.bam,test_data/sample_6.bam --fld normal --test.type SKK,CQ,KY --output test_data/new_ouput_test2  --groups 4,5 --gene.id Pcmtd1
 
 # Install dependencies
 library("Rsamtools")
@@ -39,8 +41,6 @@ bam.list2 <- ret.opts$bam2
 FLD <- ret.opts$fld
 test.type <- ret.opts$test.type
 output.dir <- ret.opts$output
-
-
 gene.name <- ret.opts$gene.id
 group.name <- ret.opts$groups
 
@@ -49,11 +49,33 @@ group.name <- ret.opts$groups
 bam.list1 <- unlist(strsplit(ret.opts$bam1, ","))
 bam.list2 <- unlist(strsplit(ret.opts$bam2, ","))
 
+# FLD
+if(is.null(ret.opts$fld))
+{
+	FLD <- "empirical"
+} else
+{
+	FLD <- ret.opts$fld
+}
+
 # test type
-test.type <- unlist(strsplit(ret.opts$test.type, ","))
+if(is.null(ret.opts$testtype))
+{
+	test.type <- "SKK"
+} else
+{
+	test.type <- ret.opts$test.type
+}
+
+if(length(test.type)>1)
+{
+	test.type <- unlist(strsplit(ret.opts$test.type, ","))
+}
+
 
 # Group
 group.name <- unlist(strsplit(ret.opts$groups, ","))
+
 
 # Main function
 IUTA(bam.list1, bam.list2, transcript.info, rep.info.1 = rep(1, length(bam.list1)), rep.info.2 = rep(1, length(bam.list2)), FLD = FLD, test.type = test.type,
@@ -66,7 +88,4 @@ estimates <- paste(output.dir,"estimates.txt",sep="/")
 pie_compare(gene.name, n1=3, geometry="Euclidean", adjust.weight = 1e-2, output.file =paste("Pieplot_", gene.name, ".pdf", sep = ""), group.name=group.name, estimates, output.screen=FALSE)
 
 # bar_comapre 
-bar_compare(gene.name, n1=3, estimates, legend.pos="topleft", output.screen=FALSE) 
-
-
-
+bar_compare(gene.name, n1=3, estimates, legend.pos="topleft", output.screen=FALSE)
