c6a5efb69250660c2df00d3cfef4ce3cc03c227f
Upendra Devisetty <upendra_35@iPlantAdmins-MacBook-Pro.local>
2016-06-16 21:27:49 -0700

    adjust plot width and font size

--- bar_compare.R
+++ bar_compare.R
@@ -9,8 +9,8 @@ bar_compare<-function(gene.name,n1,estimates.file="estimates.txt",output.file=pa
         isoforms<-estimates.gene[,2]
             usage.data<-t(estimates.gene[,-(1:2)])
 
-        pdf(output.file)
-        barplot(usage.data[complete.cases(usage.data),,drop=FALSE],main=paste("Estimated isoform usages for ",gene.name,"\n in ",sum(complete.cases(usage.data)[1:n1]),"+",sum(complete.cases(usage.data)[-(1:n1)])," samples", sep=""),names.arg=isoforms,xlab="Isoform",ylab="Relative abundance",beside=T,col=c(rep("red",n1),rep("green",nrow(usage.data)-n1))[complete.cases(usage.data)],cex.names=max(1.2-length(isoforms)/12,0.1))
+        pdf(output.file,width=15)
+        barplot(usage.data[complete.cases(usage.data),,drop=FALSE],main=paste("Estimated isoform usages for ",gene.name,"\n in ",sum(complete.cases(usage.data)[1:n1]),"+",sum(complete.cases(usage.data)[-(1:n1)])," samples", sep=""),names.arg=isoforms,xlab="Isoform",ylab="Relative abundance",beside=T,col=c(rep("red",n1),rep("green",nrow(usage.data)-n1))[complete.cases(usage.data)],cex.names=max(1.2))
         legend(legend.pos,legend=group.name,fill=c("red","green"),cex=0.9)
             dev.off()
 
